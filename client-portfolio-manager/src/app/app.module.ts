c8cbd0c4ed8c8be303534941e7c04d550cdbbdc4
matthewcaceres <matthewcaceres10@gmail.com>
2021-08-25 17:58:43 +0000

    style

--- client-portfolio-manager/src/app/app.module.ts
+++ client-portfolio-manager/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms'
 import { ChartsModule } from 'ng2-charts';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +31,8 @@ import { ChartsModule } from 'ng2-charts';
     NgbModule,
     HttpClientModule,
     FormsModule,
-   ChartsModule
+   ChartsModule,
+   NgbModule
 
   ],
   providers: [],
