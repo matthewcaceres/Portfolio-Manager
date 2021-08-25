75772f69805bb9e91f803d52906ff83a5555dffc
matthewcaceres <matthewcaceres10@gmail.com>
2021-08-25 20:43:17 +0000

    cash flow

--- client-portfolio-manager/src/app/cash-flow/cash-flow.component.ts
+++ client-portfolio-manager/src/app/cash-flow/cash-flow.component.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label, SingleDataSet } from 'ng2-charts';
+import { CashFlowService } from 'src/services/cash-flow.service';
+
+class Account {
+  transactionList: [];
+  constructor() {
+    this.transactionList = [];
+  }
+}
+
+class Transaction {
+  value: number;
+  date: Date;
+  spentOn: string;
+  cameFrom: string;
+  constructor() {
+    this.value = 0;
+    this.date = new Date();
+    this.spentOn = '';
+    this.cameFrom = '';
+  }
+}
 
 @Component({
   selector: 'app-cash-flow',
   templateUrl: './cash-flow.component.html',
-  styleUrls: ['./cash-flow.component.css']
+  styleUrls: ['./cash-flow.component.css'],
 })
 export class CashFlowComponent implements OnInit {
+  cashAccounts: Account[] = [];
+  time: string = 'week';
+  chartSpent: SingleDataSet = [];
+  chartSpentLabels: Label[] = [];
+  chartSpentMap = new Map();
+  chartEarned: SingleDataSet = [];
+  chartEarnedLabels: Label[] = [];
+  chartEarnedMap = new Map();
+  chartType: ChartType = 'doughnut';
 
-  constructor() { }
+  chartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      display: false,
+    }
+  };
+  constructor(private cashFlowService: CashFlowService) {}
 
   ngOnInit(): void {
+    this.getCashAccounts();
   }
 
+  getCashAccounts() {
+    this.cashFlowService.getTransactions(1).subscribe(
+      (data: any) => {
+        this.cashAccounts = data;
+      },
+      () => {},
+      () => {
+        this.cashAccounts.forEach((account) => {
+          account.transactionList.forEach((transaction: Transaction) => {
+            if (transaction.value > 0) {
+              if (this.chartEarnedMap.has(transaction.cameFrom))
+                this.chartEarnedMap.set(
+                  transaction.cameFrom,
+                  this.chartEarnedMap.get(transaction.cameFrom) +
+                    transaction.value
+                );
+              else
+                this.chartEarnedMap.set(
+                  transaction.cameFrom,
+                  transaction.value
+                );
+            } else {
+              if(this.chartSpentMap.has(transaction.spentOn))
+              this.chartSpentMap.set(transaction.spentOn,this.chartSpentMap.get(transaction.spentOn)+Math.abs(transaction.value))
+            else
+              this.chartSpentMap.set(transaction.spentOn,Math.abs(transaction.value));
+              
+            }
+          }
+        );
+        this.chartEarnedLabels=Array.from(this.chartEarnedMap.keys());
+        this.chartSpentLabels=Array.from(this.chartSpentMap.keys());
+        this.chartEarned=Array.from(this.chartEarnedMap.values());
+        this.chartSpent=Array.from(this.chartSpentMap.values());
+      }
+    );
+  })}
 }
--- client-portfolio-manager/src/app/net-worth/net-worth.component.ts
+++ client-portfolio-manager/src/app/net-worth/net-worth.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {  ChartOptions, ChartType } from 'chart.js';
+import { ChartOptions, ChartType } from 'chart.js';
 import { Label, SingleDataSet } from 'ng2-charts';
-import {NetWorthService} from '../../services/net-worth.service';
+import { NetWorthService } from '../../services/net-worth.service';
 @Component({
   selector: 'app-net-worth',
   templateUrl: './net-worth.component.html',
-  styleUrls: ['./net-worth.component.css']
+  styleUrls: ['./net-worth.component.css'],
 })
-
 export class NetWorthComponent implements OnInit {
-  time: string = "month";
-  chartData:SingleDataSet = [];
-  chartType: ChartType = "line"
-   lineChartLabels: Label[] = []
-   lineChartOptions: (ChartOptions) = {
+  timeFrames = ['LAST 7 DAYS', 'LAST 30 DAYS', 'LAST 3 MONTHS', 'YEAR TO DATE'];
+  timeFrame = 0;
+  time: string = 'week';
+  chartData: SingleDataSet = [];
+  chartType: ChartType = 'line';
+  lineChartLabels: Label[] = [];
+  lineChartOptions: ChartOptions = {
     responsive: true,
     legend: {
-      display: false
-  },
+      display: false,
+    },
+    tooltips: {
+      enabled: false,
+    },
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
-      xAxes: [{ticks: {
-        beginAtZero: true
-      ,display: false}}],
+      xAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            display: false,
+          },
+        },
+      ],
       yAxes: [
         {
           id: 'y-axis-0',
@@ -36,31 +45,61 @@ export class NetWorthComponent implements OnInit {
           },
           ticks: {
             fontColor: 'red',
-          }
-        }
-      ]
-    }
+          },
+        },
+      ],
+    },
   };
-  
-  constructor(private netWorthService: NetWorthService) { }
+
+  constructor(private netWorthService: NetWorthService) {}
 
   ngOnInit(): void {
     this.getData(this.time);
-    this.setDates(28);
-    
+    this.setDates(7);
   }
-  getData(time:string){
-    this.netWorthService.getNetworth(1,time)
-    .subscribe((data:any)=>{
-      this.chartData=data;
-  })}
-  setDates(days:number){
+
+  changeTime(): void {
+    if (this.timeFrame == 3) {
+      this.timeFrame = 0;
+      this.getData('week');
+      this.setDates(7)
+    } else {
+      this.timeFrame++;
+      switch(this.timeFrame){
+        case 1:
+          this.getData('month');
+          this.setDates(28);
+          break;
+        case 2:
+          this.getData('quarter');
+          this.setDates(28*3);
+          break;
+        case 3:
+          this.getData('year');
+          let today:Date = new Date();
+          let date2:Date = new Date('01/04/2021');
+          let diff = today.getTime() -date2.getTime();
+          let diffDays = Math.floor(diff/(1000*60*60*24));
+          console.log(diffDays)
+          this.setDates(diffDays)
+          break;
+      }
+    }
+  }
+
+  getData(time: string) {
+    this.netWorthService.getNetworth(1, time).subscribe((data: any) => {
+      this.chartData = data;
+    });
+  }
+  setDates(days: number) {
+    this.lineChartLabels=[];
     let day = new Date();
     let oldDay = new Date();
-    oldDay.setDate(day.getDate()-days)
-    while(days>=0){
+    oldDay.setDate(day.getDate() - days);
+    while (days >= 0) {
       this.lineChartLabels.push(oldDay.toLocaleDateString());
-      oldDay.setDate(oldDay.getDate()+1);
+      oldDay.setDate(oldDay.getDate() + 1);
       days--;
     }
   }
